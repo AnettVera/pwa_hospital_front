6d5be35ed47defb7bc1e4b8512c120e8141691e9
AnettVera <20223tn034@utez.edu.mx>
2025-12-11 10:14:29 -0600

    sw

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     // Ruta absoluta desde la raíz del proyecto
-    const swPath = '/sw.js';
+    const swPath = './../sw.js';
     navigator.serviceWorker.register(swPath)
         .then(registration => {
             console.log('Service Worker registrado con éxito:', registration.scope);
