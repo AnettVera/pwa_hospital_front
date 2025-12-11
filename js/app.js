846a004042a8aa7c09563c72a14a397fb37ec3ac
Anett Yomali Vera Carbajal <132587180+AnettVera@users.noreply.github.com>
2025-12-11 10:41:35 -0600

    Update service worker registration path

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     // Ruta absoluta desde la raíz del proyecto
-    const swPath = '/sw.js';
+    const swPath = './../sw.js';
     navigator.serviceWorker.register(swPath)
         .then(registration => {
             console.log('Service Worker registrado con éxito:', registration.scope);
@@ -9,4 +9,5 @@ if ('serviceWorker' in navigator) {
             console.log('Error al registrar el Service Worker:', error);
         });
 }
+
 
